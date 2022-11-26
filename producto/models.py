288942976e88faf527da42ca037d18e7efc44433
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-25 22:33:07 -0500

    Create modelo simple for producto

--- producto/models.py
+++ producto/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    CATEGORIAS = (("HairTools", "HairTools"), ("Accessories", "Accessories"), ("HairCare", "HairCare"), ("SAMacho", "SAMacho"))
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    valor = models.IntegerField()
+    categoria = models.CharField(max_length=12, choices = CATEGORIAS)
+    def __str__(self):
+        return '{}'.format(self.nombre)
