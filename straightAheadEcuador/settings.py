c26eeaae7a86da594979075c32beeadfd61eedb6
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-25 22:05:51 -0500

    Upload apps to settings.py

--- straightAheadEcuador/settings.py
+++ straightAheadEcuador/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cliente',
+    'factura',
+    'inventario',
+    'isla',
+    'producto'
 ]
 
 MIDDLEWARE = [
