4213ee3d895f9482263296d26c5501ccd1c3e25f
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 16:24:19 -0500

    Create model for inventario

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from isla.models import Isla
 
-# Create your models here.
+class Inventario(models.Model):
+    id = models.AutoField(primary_key=True)
+    ini = models.IntegerField()
+    ent = models.IntegerField()
+    sal = models.IntegerField()
+    fin = models.IntegerField()
+    fecha = models.DateField()
+    isla = models.ForeignKey(Isla, on_delete=models.CASCADE)
+    def __str__(self):
+        return '{}'.format(self.nombre)
