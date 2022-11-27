a53f6c541d2fb9df30e13cc71bdf2f773812c047
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 16:39:11 -0500

    Add producto to inventario

--- inventario/models.py
+++ inventario/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from isla.models import Isla
+from producto.models import Producto
 
 class Inventario(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,5 +10,6 @@ class Inventario(models.Model):
     fin = models.IntegerField()
     fecha = models.DateField()
     isla = models.ForeignKey(Isla, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     def __str__(self):
         return '{}'.format(self.nombre)
