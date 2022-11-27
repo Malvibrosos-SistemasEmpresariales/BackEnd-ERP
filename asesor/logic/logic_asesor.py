2cd36e6f5512f1d8e49e59e5b1923930f33b4240
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 18:15:22 -0500

    Add create asesor to logic

--- asesor/logic/logic_asesor.py
+++ asesor/logic/logic_asesor.py
@@ -4,3 +4,14 @@ def get_asesores():
     asesores = Asesor.objects.all()
     return asesores
 
+def create_asesor(asesor):
+    asesor_obj = Asesor(
+        id = asesor['id'],
+        nombre = asesor['nombre'],
+        apellido = asesor['apellido'],
+        correo = asesor['correo'],
+        tipoDocumento = asesor['tipoDocumento'],
+        isla = asesor['isla']
+    )
+    asesor_obj.save()
+    return asesor_obj
