368112a9a6f1cbe6c1161732d47ef5346bd588df
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 17:00:54 -0500

    Create Factura model and  FacturaDetalle

--- factura/models.py
+++ factura/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from cliente.models import Cliente
+from asesor.models import Asesor
+from producto.models import Producto
 
 class Factura(models.Model):
     id = models.AutoField(primary_key=True)
     fecha = models.DateField()
-    total = models.BigIntegerField()
+    total = models.FloatField()
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    asesor = models.CharField(max_length=50)
+    asesor = models.ForeignKey(Asesor, on_delete=models.CASCADE)
     def __str__(self):
         return '{}'.format(self.nombre)
+
+class FacturaDetalle(models.Model):
+    cantidad = models.IntegerField()
+    total = models.FloatField()
+    factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
