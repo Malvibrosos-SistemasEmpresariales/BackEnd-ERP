1262f74cf39ca9a22d61b152167f169759abc562
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 18:15:36 -0500

    Add asesores path to urls

--- straightAheadEcuador/urls.py
+++ straightAheadEcuador/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, include
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home)
+    path('home/', views.home),
+    path('asesores/', include('asesor.urls')),
 ]
