d4cf0921d8259962971d318b96ddaa38b9846499
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 16:41:45 -0500

    Create Cliente model

--- cliente/models.py
+++ cliente/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    TIPOS = (("CC", "CC"), ("TI", "TI"), ("CE", "CE"), ("Passport", "Passport"))
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.CharField(max_length=50)
+    tipoDocumento = models.CharField(max_length=8, choices = TIPOS)
+    def __str__(self):
+        return '{}'.format(self.nombre)
