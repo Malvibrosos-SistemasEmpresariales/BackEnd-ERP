1806bb7051d3a594886cbac1f35436535b03f185
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 16:44:29 -0500

    Create Factura model

--- factura/models.py
+++ factura/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cliente.models import Cliente
 
-# Create your models here.
+class Factura(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+    total = models.BigIntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    asesor = models.CharField(max_length=50)
+    def __str__(self):
+        return '{}'.format(self.nombre)
