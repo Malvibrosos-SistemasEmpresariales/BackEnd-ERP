42fa710519fcc254a20a3c48b9c2c5b322529bc1
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 18:29:37 -0500

    Add create isla and get islas to isla_logic

--- isla/logic/isla_logic.py
+++ isla/logic/isla_logic.py
@@ -3,3 +3,16 @@ from ..models import Isla
 def get_isla_by_id(id):
     isla = Isla.objects.get(pk=id)
     return isla
+
+def get_islas():
+    islas = Isla.objects.all()
+    return islas
+
+def create_isla(isla):
+    isla_obj = Isla(
+        id = isla['id'],
+        centroComercial = isla['centroComercial'],
+        ciudad = isla['ciudad']
+    )
+    isla_obj.save()
+    return isla_obj
