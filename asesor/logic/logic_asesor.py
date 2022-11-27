0193216a9b5046e6aa752a232df4612a4fda871a
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 18:16:58 -0500

    Add get asesor by id

--- asesor/logic/logic_asesor.py
+++ asesor/logic/logic_asesor.py
@@ -4,6 +4,10 @@ def get_asesores():
     asesores = Asesor.objects.all()
     return asesores
 
+def get_asesor_by_id(id):
+    asesor = Asesor.objects.get(pk=id)
+    return asesor
+
 def create_asesor(asesor):
     asesor_obj = Asesor(
         id = asesor['id'],
