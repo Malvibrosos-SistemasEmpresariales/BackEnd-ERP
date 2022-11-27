114d393d4f78d1f1c4a80d93020a4b8cffb5326f
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 16:21:07 -0500

    Create isla model

--- isla/models.py
+++ isla/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Isla(models.Model):
+    id = models.AutoField(primary_key=True)
+    centroComercial = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=50)
+    def __str__(self):
+        return '{}'.format(self.nombre)
