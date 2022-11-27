01776e49e03e6571d464cae77e544d58aa922704
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 17:30:21 -0500

    Add get petition without args for asesores

--- asesor/views.py
+++ asesor/views.py
@@ -1,3 +1,10 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.core import serializers
+from logic import lg
 
-# Create your views here.
+def asesores_view(request):
+    if request.method == 'GET':
+        asesores = lg.get_asesores()
+        asesores_dto = serializers.serialize('json', asesores)
+        return HTTPResponse(asesores_dto, content_type='application/json')
