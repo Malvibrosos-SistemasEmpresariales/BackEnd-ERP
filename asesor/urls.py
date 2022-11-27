6aa938927f6292d833875f24b7b0b09c626ab9d0
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 18:22:55 -0500

    Add path for requests with ids

--- asesor/urls.py
+++ asesor/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.asesores_view, name='asesores_view')
+    path('', views.asesores_view, name='asesores_view'),
+    path('<int:id>', views.asesor_view, name='asesor_view'),
 ]
