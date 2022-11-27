9b34f16332fb8c9a401c6baedc1b6950dbc3989c
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 16:49:41 -0500

    Create Asesor model

--- asesor/models.py
+++ asesor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from isla.models import Isla
 
-# Create your models here.
+class Asesor(models.Model):
+    TIPOS = (("CC", "CC"), ("CE", "CE"), ("Passport", "Passport"))
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.CharField(max_length=50)
+    tipoDocumento = models.CharField(max_length=8, choices = TIPOS)
+    isla = models.ForeignKey(Isla, on_delete=models.CASCADE)
+    def __str__(self):
+        return '{}'.format(self.nombre)
