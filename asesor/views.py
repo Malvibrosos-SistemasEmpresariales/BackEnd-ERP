32d7898d9077a5fc88198851284fc13f7b9eb796
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 18:23:11 -0500

    Add view for post and get by id

--- asesor/views.py
+++ asesor/views.py
@@ -1,3 +1,4 @@
+import json
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.core import serializers
@@ -8,3 +9,13 @@ def asesores_view(request):
         asesores = lg.get_asesores()
         asesores_dto = serializers.serialize('json', asesores)
         return HTTPResponse(asesores_dto, content_type='application/json')
+    elif request.method == 'POST':
+        asesor_dto = lg.create_asesor(json.loads(request.body))
+        asesor = serializers.serialize('json', [asesor_dto])
+        return HTTPResponse(asesor, content_type='application/json')
+
+def asesor_view(request, id):
+    if request.method == 'GET':
+        asesor = lg.get_asesor_by_id(id)
+        asesor_dto = serializers.serialize('json', [asesor])
+        return HTTPResponse(asesor_dto, content_type='application/json')
