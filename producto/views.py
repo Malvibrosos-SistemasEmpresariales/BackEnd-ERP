b40e2c168f715e45a071daecca5ce341fa6c03b1
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 19:18:19 -0500

    Create views for POST and GET requests

--- producto/views.py
+++ producto/views.py
@@ -1,3 +1,22 @@
+import json
+from django.core import serializers
+from http.client import HTTPResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .logic import producto_logic as lg
+
+def productos_view(request):
+    if request.method == 'GET':
+        productos = lg.get_productos()
+        productos_dto = serializers.serialize('json', productos)
+        return HTTPResponse(productos_dto, content_type='application/json')
+    elif request.method == 'POST':
+        producto_dto = lg.create_producto(json.loads(request.body))
+        producto = serializers.serialize('json', [producto_dto])
+        return HTTPResponse(producto, content_type='application/json')
+
+def producto_view(request, id):
+    if request.method == 'GET':
+        producto = lg.get_producto_by_id(id)
+        producto_dto = serializers.serialize('json', [producto])
+        return HTTPResponse(producto_dto, content_type='application/json')
