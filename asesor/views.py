fd59de67227a8da2224db2508ce5a30335687c85
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-28 16:19:51 -0500

    Fix content type arg

--- asesor/views.py
+++ asesor/views.py
@@ -1,5 +1,5 @@
 import json
-from http.client import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.core import serializers
 from .logic import logic_asesor as lg
@@ -8,14 +8,14 @@ def asesores_view(request):
     if request.method == 'GET':
         asesores = lg.get_asesores()
         asesores_dto = serializers.serialize('json', asesores)
-        return HTTPResponse(asesores_dto, content_type='application/json')
+        return HttpResponse(asesores_dto, 'application/json')
     elif request.method == 'POST':
         asesor_dto = lg.create_asesor(json.loads(request.body))
         asesor = serializers.serialize('json', [asesor_dto])
-        return HTTPResponse(asesor, content_type='application/json')
+        return HttpResponse(asesor,'application/json')
 
 def asesor_view(request, id):
     if request.method == 'GET':
         asesor = lg.get_asesor_by_id(id)
         asesor_dto = serializers.serialize('json', [asesor])
-        return HTTPResponse(asesor_dto, content_type='application/json')
+        return HttpResponse(asesor_dto,'application/json')
