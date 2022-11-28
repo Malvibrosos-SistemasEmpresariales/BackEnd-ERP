1aabea8b385dd9259889fbc0fc5c10a410bac336
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 19:19:38 -0500

    Create urls for product requests

---
+++ producto/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.islas_view, name='islas_view'), 
+    path('<int:id>', views.isla_view, name='isla_view'),
+]
