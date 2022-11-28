1ef4205ae185be216faa498b9a5f9928191ffab7
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-28 13:39:51 -0500

    Add CRSF exemption to views cliente

--- cliente/views.py
+++ cliente/views.py
@@ -1,21 +1,24 @@
 import json
 from django.shortcuts import render
 from django.core import serializers
-from http.client import HTTPResponse
+from django.http import HttpResponse
 from .logic import cliente_logic as lg
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def clientes_view(request):
     if request.method == 'GET':
         clientes = lg.get_clientes()
         clientes_dto = serializers.serialize('json', clientes)
-        return HTTPResponse(clientes_dto, content_type='application/json')
+        return HttpResponse(clientes_dto,'application/json')
     elif request.method == 'POST':
         cliente_dto = lg.create_cliente(json.loads(request.body))
         cliente = serializers.serialize('json', [cliente_dto])
-        return HTTPResponse(cliente, content_type='application/json')
+        return HttpResponse(cliente, 'application/json')
 
+@csrf_exempt
 def cliente_view(request, id):
     if request.method == 'GET':
         cliente = lg.get_cliente_by_id(id)
         cliente_dto = serializers.serialize('json', [cliente])
-        return HTTPResponse(cliente_dto, content_type='application/json')
+        return HttpResponse(cliente_dto, content_type='application/json')
