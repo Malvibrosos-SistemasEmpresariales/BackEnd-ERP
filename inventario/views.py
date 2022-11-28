a73138c95638b5deeab13e843c84964754610f2c
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 19:39:39 -0500

    Update inventario views

--- inventario/views.py
+++ inventario/views.py
@@ -1,3 +1,21 @@
+import json
 from django.shortcuts import render
+from django.core import serializers
+from http.client import HTTPResponse
+from .logic import inventario_logic as lg
 
-# Create your views here.
+def inventarios_view(request):
+    if request.method == 'GET':
+        inventarios = lg.get_inventarios()
+        inventarios_dto = serializers.serialize('json', inventarios)
+        return HTTPResponse(inventarios_dto, content_type='application/json')
+    elif request.method == 'POST':
+        inventario_dto = lg.create_inventario(json.loads(request.body))
+        inventario = serializers.serialize('json', [inventario_dto])
+        return HTTPResponse(inventario, content_type='application/json')
+
+def inventario_view(request, id):
+    if request.method == 'GET':
+        inventario = lg.get_inventario_by_id(id)
+        inventario_dto = serializers.serialize('json', [inventario])
+        return HTTPResponse(inventario_dto, content_type='application/json')
