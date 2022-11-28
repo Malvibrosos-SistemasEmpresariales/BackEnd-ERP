1ac9789e476face072d9735e14ca35bf946ee314
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-28 13:43:22 -0500

    Change HTTPResponse import

--- isla/views.py
+++ isla/views.py
@@ -1,7 +1,7 @@
 import json
 from django.shortcuts import render
 from django.core import serializers
-from http.client import HTTPResponse
+from django.http import HttpResponse
 from .logic import isla_logic as lg
 from django.views.decorators.csrf import csrf_exempt
 
@@ -10,15 +10,15 @@ def islas_view(request):
     if request.method == 'GET':
         islas = lg.get_islas()
         islas_dto = serializers.serialize('json', islas)
-        return HTTPResponse(islas_dto, 'application/json')
+        return HttpResponse(islas_dto, 'application/json')
     elif request.method == 'POST':
         isla_dto = lg.create_isla(json.loads(request.body))
         isla = serializers.serialize('json', [isla_dto])
-        return HTTPResponse(isla, 'application/json')
+        return HttpResponse(isla, 'application/json')
 
 @csrf_exempt
 def isla_view(request, id):
     if request.method == 'GET':
         isla = lg.get_isla_by_id(id)
         isla_dto = serializers.serialize('json', [isla])
-        return HTTPResponse(isla_dto, 'application/json')
+        return HttpResponse(isla_dto, 'application/json')
