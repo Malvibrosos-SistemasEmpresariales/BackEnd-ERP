defa8bfa8ac59bb681c3e1acdf0002d76b7a68e9
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-28 13:30:36 -0500

    Fix HTTPResponse import

--- factura/views.py
+++ factura/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 import json
 from django.core import serializers
-from http.client import HTTPResponse
+from django.http import HttpResponse
 from .logic import factura_logic as lg
 
 def facturas_view(request):
     if request.method == 'GET':
         facturas = lg.get_facturas()
         facturas_dto = serializers.serialize('json', facturas)
-        return HTTPResponse(facturas_dto, content_type='application/json')
+        return HttpResponse(facturas_dto, 'application/json')
     elif request.method == 'POST':
         factura_dto = lg.create_factura(json.loads(request.body))
         factura = serializers.serialize('json', [factura_dto])
-        return HTTPResponse(factura, content_type='application/json')
+        return HttpResponse(factura, 'application/json')
 
 def factura_view(request, id):
     if request.method == 'GET':
         factura = lg.get_factura_by_id(id)
         factura_dto = serializers.serialize('json', [factura])
-        return HTTPResponse(factura_dto, content_type='application/json')
+        return HttpResponse(factura_dto, 'application/json')
