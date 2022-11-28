ed790da93fbfca141e23b1f13c0c0c3bc5c51421
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-28 16:39:10 -0500

    Make migrations to product

--- producto/models.py
+++ producto/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Producto(models.Model):
     CATEGORIAS = (("HairTools", "HairTools"), ("Accessories", "Accessories"), ("HairCare", "HairCare"), ("SAMacho", "SAMacho"))
-    id = models.CharField(max_length=100, primary_key=True, unique=True)
+    codigo = models.CharField(max_length=100, primary_key=True, unique=True)
     nombre = models.CharField(max_length=50)
     valor = models.IntegerField()
     categoria = models.CharField(max_length=12, choices = CATEGORIAS)
