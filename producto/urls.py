684494ad841fa12a9ca234b4ade83c71aa30d3f1
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 19:51:10 -0500

    Fix urls error in producto urls

--- producto/urls.py
+++ producto/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.islas_view, name='islas_view'), 
-    path('<int:id>', views.isla_view, name='isla_view'),
+    path('', views.productos_view, name='productos_view'), 
+    path('<int:id>', views.producto_view, name='producto_view'),
 ]
