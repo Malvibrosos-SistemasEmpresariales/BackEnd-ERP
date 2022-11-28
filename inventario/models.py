ac3f03c2d82bfca7d176aebed6c92f86420d6e9b
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-27 19:06:28 -0500

    Create invetarioMovimientos model

--- inventario/models.py
+++ inventario/models.py
@@ -4,12 +4,15 @@ from producto.models import Producto
 
 class Inventario(models.Model):
     id = models.AutoField(primary_key=True)
-    ini = models.IntegerField()
-    ent = models.IntegerField()
-    sal = models.IntegerField()
-    fin = models.IntegerField()
-    fecha = models.DateField()
+    cantidad = models.IntegerField()
     isla = models.ForeignKey(Isla, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
+class InventarioMovimientos(models.Model):
+    cantidad = models.IntegerField()
+    fecha = models.DateField()
+    inventario = models.ForeignKey(Inventario, on_delete=models.CASCADE)
     def __str__(self):
         return '{}'.format(self.nombre)
