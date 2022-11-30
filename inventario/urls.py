feba796bd7f3d127cc66cf4d64f1112ddcc40e9b
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 19:49:02 -0500

    Change request input type

--- inventario/urls.py
+++ inventario/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.inventarios_view, name='inventarios_view'),
     path('<int:id>', views.inventario_view, name='inventario_view'),
-    path('movimientos/<int:id>', views.movimientos_productos_view, name='movimientos_productos_view'),
+    path('movimientos/<str:id>', views.movimientos_productos_view, name='movimientos_productos_view'),
     path('movimientos/', views.movimientos_view, name='movimientos_view'),
 ]
