e7116fb0b0a8fdc13204cde9fbcbd1c6eeee7868
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 19:24:50 -0500

    Fix HttpResponse import

--- inventario/views.py
+++ inventario/views.py
@@ -1,21 +1,21 @@
 import json
 from django.shortcuts import render
 from django.core import serializers
-from http.client import HTTPResponse
+from django.http import HttpResponse
 from .logic import inventario_logic as lg
 
 def inventarios_view(request):
     if request.method == 'GET':
         inventarios = lg.get_inventarios()
         inventarios_dto = serializers.serialize('json', inventarios)
-        return HTTPResponse(inventarios_dto, 'application/json')
+        return HttpResponse(inventarios_dto, 'application/json')
     elif request.method == 'POST':
         inventario_dto = lg.create_inventario(json.loads(request.body))
         inventario = serializers.serialize('json', [inventario_dto])
-        return HTTPResponse(inventario, 'application/json')
+        return HttpResponse(inventario, 'application/json')
 
 def inventario_view(request, id):
     if request.method == 'GET':
         inventario = lg.get_inventario_by_id(id)
         inventario_dto = serializers.serialize('json', [inventario])
-        return HTTPResponse(inventario_dto,'application/json')
+        return HttpResponse(inventario_dto,'application/json')
