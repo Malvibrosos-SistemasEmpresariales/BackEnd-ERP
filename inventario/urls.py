c4e7185e22980e82279608ccb827b5e4f17d31e2
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 19:41:34 -0500

    Add slash to movimientos url

--- inventario/urls.py
+++ inventario/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.inventarios_view, name='inventarios_view'),
     path('<int:id>', views.inventario_view, name='inventario_view'),
     path('movimientos/<int:id>', views.movimientos_productos_view, name='movimientos_productos_view'),
-    path('movimientos', views.movimientos_view, name='movimientos_view'),
+    path('movimientos/', views.movimientos_view, name='movimientos_view'),
 ]
