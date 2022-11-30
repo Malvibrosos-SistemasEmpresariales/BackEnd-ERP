2e741c08a2a677bebcaeaa6b44f361b6eb108291
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 19:24:23 -0500

    Quitar content_type field

--- inventario/views.py
+++ inventario/views.py
@@ -8,14 +8,14 @@ def inventarios_view(request):
     if request.method == 'GET':
         inventarios = lg.get_inventarios()
         inventarios_dto = serializers.serialize('json', inventarios)
-        return HTTPResponse(inventarios_dto, content_type='application/json')
+        return HTTPResponse(inventarios_dto, 'application/json')
     elif request.method == 'POST':
         inventario_dto = lg.create_inventario(json.loads(request.body))
         inventario = serializers.serialize('json', [inventario_dto])
-        return HTTPResponse(inventario, content_type='application/json')
+        return HTTPResponse(inventario, 'application/json')
 
 def inventario_view(request, id):
     if request.method == 'GET':
         inventario = lg.get_inventario_by_id(id)
         inventario_dto = serializers.serialize('json', [inventario])
-        return HTTPResponse(inventario_dto, content_type='application/json')
+        return HTTPResponse(inventario_dto,'application/json')
