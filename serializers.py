d1d34ef43712a7e065bb4e9e1e5f956dc072cc1c
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-29 22:19:30 -0500

    Create serializers for JSON code

---
+++ serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers 
+from factura.models import Factura, FacturaDetalle
+
+class FacturaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Factura
+        fields = ('id',
+                  'fecha',
+                  'total',
+                  'cliente',
+                  'asesor')
+
+class FacturaDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FacturaDetalle
+        fields = ('cantidad',
+                  'total',
+                  'factura',
+                  'producto')
