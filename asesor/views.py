d6cd1619ace011daa2e3da3e0099b0baabd967e7
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-12-01 17:52:32 -0500

    JsonResponse

--- asesor/views.py
+++ asesor/views.py
@@ -2,7 +2,7 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core import serializers
-
+from django.http.response import JsonResponse
 from serializers import AsesorSerializer
 from .logic import logic_asesor as lg
 from django.views.decorators.csrf import csrf_exempt
@@ -12,7 +12,7 @@ def asesores_view(request):
     if request.method == 'GET':
         asesores = lg.get_asesores()
         asesores_dto = AsesorSerializer(asesores, many=True)
-        return HttpResponse(asesores_dto.data, 'application/json')
+        return JsonResponse(asesores_dto.data, safe = False)
     elif request.method == 'POST':
         asesor_dto = lg.create_asesor(json.loads(request.body))
         asesor = serializers.serialize('json', [asesor_dto])
@@ -23,4 +23,4 @@ def asesor_view(request, id):
     if request.method == 'GET':
         asesor = lg.get_asesor_by_id(id)
         asesor_dto = AsesorSerializer(asesor, many=False)
-        return HttpResponse(asesor_dto.data,'application/json')
+        return JsonResponse(asesor_dto.data,safe=False)
--- inventario/views.py
+++ inventario/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse
 from .logic import inventario_logic as lg
+from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from serializers import InventarioSerializer, InventarioDetailSerializer
 
 @csrf_exempt
 def inventarios_view(request):
     if request.method == 'GET':
         inventarios = lg.get_inventarios()
-        inventarios_dto = serializers.serialize('json', inventarios)
-        return HttpResponse(inventarios_dto, 'application/json')
+        inventarios_dto = InventarioSerializer(inventarios, many=True)
+        return JsonResponse(inventarios_dto.data, safe=False)
     elif request.method == 'POST':
         inventario_dto = lg.create_inventario(json.loads(request.body))
         inventario = serializers.serialize('json', [inventario_dto])
@@ -20,15 +22,20 @@ def inventarios_view(request):
 def inventario_view(request, id):
     if request.method == 'GET':
         inventario = lg.get_inventario_by_id(id)
-        inventario_dto = serializers.serialize('json', [inventario])
-        return HttpResponse(inventario_dto,'application/json')
+        inventario_detail = lg.get_movimientos_by_id_producto(id)
+        list_movimientos = []
+        for movimiento in inventario_detail:
+            list_movimientos.append(InventarioDetailSerializer(movimiento).data)
+        inventario_dto = InventarioSerializer(inventario, many=False).data
+        inventario_dto['movimientos'] = list_movimientos
+        return JsonResponse(inventario_dto, safe=False)
 
 @csrf_exempt
 def movimientos_productos_view(request, id):
     if request.method == 'GET':
         movimientos = lg.get_movimientos_by_id_producto(id)
-        movimientos_dto = serializers.serialize('json', movimientos)
-        return HttpResponse(movimientos_dto, 'application/json')
+        movimientos_dto = InventarioDetailSerializer(movimientos, many=True)
+        return JsonResponse(movimientos_dto.data, safe=False)
 
 @csrf_exempt
 def movimientos_view(request):
