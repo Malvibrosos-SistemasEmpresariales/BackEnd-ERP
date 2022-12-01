7b6a7f2dbc44674f4ed2c87343fa6ca76a283eae
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-12-01 17:42:05 -0500

    Change asesor serializer in get requests

--- asesor/views.py
+++ asesor/views.py
@@ -2,6 +2,8 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core import serializers
+
+from serializers import AsesorSerializer
 from .logic import logic_asesor as lg
 from django.views.decorators.csrf import csrf_exempt
 
@@ -9,8 +11,8 @@ from django.views.decorators.csrf import csrf_exempt
 def asesores_view(request):
     if request.method == 'GET':
         asesores = lg.get_asesores()
-        asesores_dto = serializers.serialize('json', asesores)
-        return HttpResponse(asesores_dto, 'application/json')
+        asesores_dto = AsesorSerializer(asesores, many=True)
+        return HttpResponse(asesores_dto.data, 'application/json')
     elif request.method == 'POST':
         asesor_dto = lg.create_asesor(json.loads(request.body))
         asesor = serializers.serialize('json', [asesor_dto])
@@ -20,5 +22,5 @@ def asesores_view(request):
 def asesor_view(request, id):
     if request.method == 'GET':
         asesor = lg.get_asesor_by_id(id)
-        asesor_dto = serializers.serialize('json', [asesor])
-        return HttpResponse(asesor_dto,'application/json')
+        asesor_dto = AsesorSerializer(asesor, many=False)
+        return HttpResponse(asesor_dto.data,'application/json')
